43e30f171309cd633afcb55a98177184186cc766
j717273419 <717273419@qq.com>
2023-11-06 10:03:57 +0800

    在typescript中使用感叹号！

---
+++ TypeScript/TS-01-Exclamation-Point.js
@@ -0,0 +1,6 @@
+var myVar = "hello world";
+console.log(myVar);
+var myStr = myVar;
+console.log(myStr);
+var myStr2 = myVar;
+console.log(myStr2);
---
+++ TypeScript/TS-01-Exclamation-Point.ts
@@ -0,0 +1,8 @@
+let myVar: any = "hello world";
+console.log(myVar)
+
+let myStr: string = myVar
+console.log(myStr)
+
+let myStr2: string = myVar!
+console.log(myStr2)
